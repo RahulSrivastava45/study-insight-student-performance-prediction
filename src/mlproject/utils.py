d45ccd26ba9bfd7fd6901a3d3ebfa3626e113e0d
Rahul Srivastava <rahulkumarsrivastava90@gmail.com>
2026-08-02 14:23:17 +0530

    Few changes in some folders

--- src/mlproject/utils.py
+++ src/mlproject/utils.py
@@ -4,39 +4,44 @@ from src.mlproject.exception import CustomException
 from src.mlproject.logger import logging
 import pandas as pd
 from dotenv import load_dotenv
-from sklearn.model_selection import GridSearchCV
-from sklearn.metrics import r2_score
 import pymysql
-
 import pickle
-import numpy as np
 
 load_dotenv()
 
-host=os.getenv("host")
-user=os.getenv("user")
-password=os.getenv("password")
-db=os.getenv('db')
-
+host = os.getenv("host")
+user = os.getenv("user")
+password = os.getenv("password")
+db = os.getenv("db")
 
 
 def read_sql_data():
     logging.info("Reading SQL database started")
     try:
-        mydb=pymysql.connect(
+        mydb = pymysql.connect(
             host=host,
             user=user,
             password=password,
             db=db
         )
-        logging.info("Connection Established",mydb)
-        df=pd.read_sql_query('Select * from students',mydb)
-        print(df.head())
+
+        logging.info("Connection Established")
+        df = pd.read_sql_query("SELECT * FROM students", mydb)
 
         return df
 
-
-
     except Exception as ex:
-        raise CustomException(ex)
-    
+        raise CustomException(ex, sys)
+
+
+def save_object(file_path, obj):
+    try:
+        dir_path = os.path.dirname(file_path)
+
+        os.makedirs(dir_path, exist_ok=True)
+
+        with open(file_path, "wb") as file_obj:
+            pickle.dump(obj, file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
